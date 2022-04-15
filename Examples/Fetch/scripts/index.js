4ee1a8f76b3196970812b56d8e1d2e5a5270a208
Macrophage001 <zerosalvation@gmail.com>
2022-04-15 09:16:59 -0400

    - cleaned up some of the code, added some comments

--- Examples/Fetch/scripts/index.js
+++ Examples/Fetch/scripts/index.js
@@ -14,7 +14,7 @@ const updatePostPreviews = (posts) => {
     articleSection.innerHTML = "";
     console.log(posts);
 
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < posts.length; i++) {
         let title = posts[i].title;
         let content = posts[i].content;
         articleSection.innerHTML += articleElement( title, content ); 
@@ -44,12 +44,15 @@ window.onload = (e) => {
             // Making a fetch request to 'https://jsonplaceholder.typicode.com/posts' returns
             // 100 posts, that's too much so we just get 5 posts instead.
             for (let i = 0; i < 5; i++) {
+                // The 'post' objects have to much info in them that I don't need
+                // So here I only take what's necessary for my purposes (The Title and Body of each post).
+
                 let post = postsAsJson[i];
                 let title = post.title;
                 let content = post.body;
 
                 posts.push({title, content});
-            }}) // Here I only take what I need (The Title and Body of each post)
+            }})
         .catch(err => console.log(err.message)) // In case there is an error, just print it out to the console.
         .finally(() => updatePostPreviews(posts)); // After all of that work is done, I want it to make sure it updates the post previews!
 };
