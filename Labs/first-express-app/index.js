6be7302965c7d6fea75e5a128828d1ff2bf68d25
Macrophage001 <champend02@gmail.com>
2022-05-13 13:40:47 -0400

    - finished 'Greetings', and 'Tip Calculator'

---
+++ Labs/first-express-app/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 5000;
+
+let magic8BallResponses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"];
+
+let greetings = [
+    (name) => `Hello, ${name}`,
+    (name) => `What's up, ${name}`,
+    (name) => `${name}!, It's great to see you!`,
+    (name) => `${name}!, Long time no see!`
+]
+
+app.get('/greeting', (req, resp) => {
+    resp.send('Hello, Stranger!');
+});
+app.get('/greeting/:name', (req, resp) => {
+    let selectedGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+    resp.send(selectedGreeting(req.params.name));
+});
+app.get('/tip/:total/:tipPercentage', (req, resp) => {
+    resp.send(`Total Tip: ${req.params.total * req.params.tipPercentage}$`);
+});
+
+app.listen(port, () => console.log('Listening on port: ' + port));
