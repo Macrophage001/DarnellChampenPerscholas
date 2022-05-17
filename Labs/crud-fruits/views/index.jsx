b67992c1d7abc45840b49ffa90dafe20112fefab
Macrophage001 <champend02@gmail.com>
2022-05-17 15:16:08 -0400

    - added mongodb support

--- Labs/crud-fruits/views/index.jsx
+++ Labs/crud-fruits/views/index.jsx
@@ -3,9 +3,7 @@ import React from 'react'
 const Index = ({ fruits }) => (
     <div>
         <h1>Fruits Index Page</h1>
-        <nav>
-            <a href="/fruits/new">Create a New Fruit</a>
-        </nav>
+        <nav> <a href="/fruits/new">Create a New Fruit</a> </nav>
         <ul>
             {fruits.map((fruit, i) => <li>The {' '} <a href={ `/fruits/${i}` }>{fruit.name}</a> {' '} is {fruit.color} and is { fruit.readyToEat ? 'ready to eat!' : 'not ready to eat!' }</li>)}
         </ul>
