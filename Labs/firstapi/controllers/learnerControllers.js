fafe049576ae08967827abe07239ca535b50b54f
Macrophage001 <champend02@gmail.com>
2022-05-18 14:58:47 -0400

    - fixing some exports

--- Labs/firstapi/controllers/learnerControllers.js
+++ Labs/firstapi/controllers/learnerControllers.js
@@ -1,18 +1,17 @@
 const getAllLearners = (req, resp) => resp.status(500).send('Getting All Learners');
-const createLearner = (req, resp) => {
-    resp
-        .status(200)
-        .send(req.body);
-}
 
-const getSingleLearner = (req, resp) => resp.status(500).send(`Getting learner at ${req.params.id}`);
+const createLearner = (req, resp) => resp
+    .status(200)
+    .send(req.body);
+
+const getSingleLearner = (req, resp) => { return resp.status(500).send(`Getting learner at ${req.params.id}`); }
 const updateLearner = (req, resp) => resp.status(500).send('Updating Learner at ${req.params.id}');
 const deleteLearner = (req, resp) => resp.status(500).send('Route Undefined');
 
-module.exports = {
+module.exports = { 
     getAllLearners,
     createLearner,
-    getSingleLearner,
-    updateLearner,
-    deleteLearner
-}
+    getSingleLearner, 
+    updateLearner, 
+    deleteLearner 
+};
